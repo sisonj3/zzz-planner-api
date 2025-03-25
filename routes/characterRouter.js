805698dae7072e4c07cbafdbc314625910ad984f
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-03-24 17:04:21 -0700

    Reverted last change

--- routes/characterRouter.js
+++ routes/characterRouter.js
@@ -6,7 +6,7 @@ const { Router } = require('express');
 const characterRouter = Router();
 
 // Get all character names
-characterRouter.get("/", [characterController.getCharacterNames]);
+characterRouter.get("/", [loginController.verifyToken, characterController.getCharacterNames]);
 
 // Get character by name
 characterRouter.get("/:name", [loginController.verifyToken, characterController.getCharacterByName]);
