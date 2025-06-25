d77393f1a460ffc4d6f9bf76b349222230797f4a
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-06-24 20:14:49 -0700

    Changed prisma schema

---
+++ prisma/migrations/20250625031423_account_only/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Character` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Wengine` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Character";
+
+-- DropTable
+DROP TABLE "Wengine";
+
+-- DropEnum
+DROP TYPE "Attribute";
+
+-- DropEnum
+DROP TYPE "Expert";
+
+-- DropEnum
+DROP TYPE "Rank";
+
+-- DropEnum
+DROP TYPE "Role";
+
+-- DropEnum
+DROP TYPE "Weekly";
